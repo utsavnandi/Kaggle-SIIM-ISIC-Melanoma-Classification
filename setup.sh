4f0f45585654930766d9316c1c669b76997a5b2f
Utsav Nandi <utsav.nandi@gmail.com>
2020-06-08 10:01:27 +0530

    changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,21 @@
+pip uninstall torch
+pip install torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 pip uninstall kaggle -y
 pip install kaggle==1.5.6 -q
 pip install -U git+https://github.com/albu/albumentations -q
 pip install -U git+https://github.com/rwightman/pytorch-image-models -q
 pip install neptune-client -q
-git clone https://github.com/galatolofederico/pytorch-balanced-batch.git
-mv ./pytorch-balanced-batch/sampler.py ./sampler.py
-rm -rf ./pytorch-balanced-batch/
 mkdir ~/.kaggle/
-cp ./kaggle.json  ~/.kaggle/kaggle.json
+cp /home/utsav_nandi/kaggle.json  ~/.kaggle/kaggle.json
 chmod 600 ~/.kaggle/kaggle.json
-kaggle datasets download tunguz/siimisic-melanoma-resized-images -f x_train_224.npy
-kaggle datasets download tunguz/siimisic-melanoma-resized-images -f x_test_224.npy
-unzip ./680899%2F1200702%2Fcompressed%2Fx_train_224.npy.zip -d ./data/
-unzip ./680899%2F1200702%2Fcompressed%2Fx_test_224.npy.zip -d ./data/
-rm ./680899%2F1200702%2Fcompressed%2Fx_train_224.npy.zip
-rm ./680899%2F1200702%2Fcompressed%2Fx_test_224.npy.zip
+kaggle datasets download -d shonenkov/melanoma-merged-external-data-512x512-jpeg
+unzip melanoma-merged-external-data-512x512-jpeg.zip -d /home/utsav_nandi/data/
+rm /home/utsav_nandi/melanoma-merged-external-data-512x512-jpeg.zip
 kaggle competitions download siim-isic-melanoma-classification -f sample_submission.csv
 kaggle competitions download siim-isic-melanoma-classification -f test.csv
 kaggle competitions download siim-isic-melanoma-classification -f train.csv
-unzip train.csv -d ./data/
-mv ./test.csv ./data/
-mv ./sample_submission.csv ./data/
-rm train.csv.zip
-mkdir ./logs/
+unzip /home/utsav_nandi/train.csv -d /home/utsav_nandi/data/
+mv /home/utsav_nandi/test.csv /home/utsav_nandi/data/
+mv /home/utsav_nandi/sample_submission.csv /home/utsav_nandi/data/
+rm /home/utsav_nandi/train.csv.zip
+mkdir /home/utsav_nandi/logs/
